c4b9b68cdfd078c404f917b4dc4456891a59409c
Dikshanta <3108dikshanta@gmail.com>
2025-04-23 10:41:06 +0545

    Profile page added

--- app/src/main/java/com/example/softwareengine/ProfileActivity.kt
+++ app/src/main/java/com/example/softwareengine/ProfileActivity.kt
@@ -5,17 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.softwareengine.ui.theme.SoftwareEngineTheme
 
 class ProfileActivity : ComponentActivity() {
@@ -36,9 +52,57 @@ fun ProfileBody(innerPadding: PaddingValues) {
         modifier = Modifier
             .padding(innerPadding)
             .fillMaxSize()
-            .background(color = Color.Red)
+            .background(color = Color.White)
     ) {
-        Text(text = "Welcome to Profile Page", color = Color.White)
+        Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp, vertical = 10.dp),
+            verticalAlignment = Alignment.CenterVertically ,
+            horizontalArrangement = Arrangement.SpaceBetween
+
+
+        )
+
+        {
+
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowLeft,
+                tint = Color.Red,
+                contentDescription = null,
+                modifier = Modifier
+                    .height(28.dp)
+                    .width(28.dp)
+                    .clickable {
+
+                    }
+            )
+
+
+            Text(text = "Dikshanta", style = TextStyle(
+                fontSize = 28.sp,
+                textDecoration =  TextDecoration.Underline,
+                fontWeight = FontWeight.Bold,
+                // fontStyle = FontStyle.Italic
+
+            ))
+
+
+            Icon(
+                painter = painterResource(R.drawable.baseline_more_horiz_24),
+                tint = Color.Red,
+                contentDescription = null,
+                modifier = Modifier
+                    .height(28.dp)
+                    .width(28.dp)
+                    .clickable {
+
+                    }
+            )
+
+
+
+
+
+
+        }
     }
 }
 
